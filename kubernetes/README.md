5485617945405c1a7e160e588c5d26e12941baa0
Viktor Farcic <viktor@farcic.com>
2015-03-18 09:13:37 +0100

    Started Kubernetes

---
+++ kubernetes/README.md
@@ -0,0 +1,6 @@
+```bash
+vagrant up master
+vagrant up minion1
+vagrant up minion2
+vagrant up minion3
+```
---
+++ kubernetes/bootstrap_master.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Installing Ansible..."
+apt-get install -y software-properties-common
+apt-add-repository ppa:ansible/ansible
+apt-get update
+apt-get install -y ansible
+echo "Installing git..."
+apt-get install -y git
+echo "Installing Docker..."
+apt-get install -y docker.io python-pip
+pip install docker-py==0.4.0
+echo "Installing Kubernetes..."
+cd /vagrant
+git clone https://github.com/GoogleCloudPlatform/kubernetes.git
+cd /vagrant/kubernetes/cluster/ubuntu-cluster
+./build.sh
+cp /vagrant/kubernetes/cluster/ubuntu_cluster/binaries/* /opt/bin
